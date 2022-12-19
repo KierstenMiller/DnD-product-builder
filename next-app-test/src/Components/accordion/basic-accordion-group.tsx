376fc9514fbb1adfcfea8a8bf85b6ea42de80f2e
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2022-12-18 18:06:37 -0600

    stubbing in group accordion

---
+++ next-app-test/src/Components/accordion/basic-accordion-group.tsx
@@ -0,0 +1,15 @@
+import classNames from "classnames";
+
+interface props {
+    children: React.ReactNode; // TODO: can I type it to be just an accordion component / array of accordion components
+    className?: string;
+}
+
+const BasicAccordionGroup = ({children, className}: props) => {
+    // TODO: implement grouped accordions keyboard input management
+    return (
+        <div className={classNames(`basic-accordion-group`, className && className)}>
+            {children}
+        </div>
+    )
+}
