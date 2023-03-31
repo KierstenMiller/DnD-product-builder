090420ae9aa06dc0d6f335a564d385b64068d92d
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2023-03-31 16:41:20 -0500

    working again

--- next-app-test/src/page-components/build-your-own/aggulative-stacks.model.tsx
+++ next-app-test/src/page-components/build-your-own/aggulative-stacks.model.tsx
@@ -1,5 +1,5 @@
 import { groupKeyValues } from "-/Components/modifier/modifier.types";
-import { makeObservable, observable, computed, action } from "mobx"
+import { makeAutoObservable, makeObservable, observable, computed, action } from "mobx"
 import { aggulativeStacksT, configT, pieceI } from "./build-your-own.types";
 import { overrideConfig } from "./build-your-own.util";
 
@@ -19,64 +19,74 @@ const findIndex2D = (stacks: aggulativeStacksT, id: string) => {
 //     return globalConfig.map(c => c.groupKey === groupKeyValues.unique ? { ...c } : c);
 // }
 
-export class AggulativeStacks {
+class Piece {
+    id
     config
-    stacks
-    constructor({ config, stacks: stacksData }: { config: configT, stacks: aggulativeStacksT }) {
+    constructor({id, config}: {id: string, config: configT}){
+        this.id = id;
         this.config = config;
-        this.stacks = [];
-        console.log('this.config', this.config);
         makeObservable(this, {
             config: observable,
-            // stacksData: observable,
-            stacks: observable.ref, // lower parts should decide observability
+            setConfig: action.bound,
+        })
+    }
+    setConfig = (newConfig: configT) => {
+        console.log('setting config with', newConfig);
+        this.config = newConfig;
+    };
+}
+
+export class AggulativeStacks {
+    config
+    stacksData
+    // stacks
+    constructor({ config, stacks: stacksData }: { config: configT, stacks: aggulativeStacksT }) {
+        this.config = config;
+        this.stacksData = stacksData;
+        makeAutoObservable(this, {
+            stacks: computed, // lower parts should decide observability
             setConfig: action.bound,
             addStack: action.bound,
             addToStack: action.bound,
             findAndRemoveBlock: action.bound,
             generatePiece: action.bound,
             clearEmptyStacks: action.bound,
-        }) 
-        // BIG WHY: NEED CONFIG TO BE OBSERVABLE
-        console.log('after', this.config);
-        stacksData.forEach(s => s.forEach(b => b.piece.config = this.config.map(c => c.groupKey === groupKeyValues.unique ? {...(b.piece.config.find(pC => pC.id === c.id))} : c)))
-        this.stacks = stacksData;
+        })
+        
+        // .map(s => s.map(b => ({...b, piece: new Piece({id: b.piece.id, config: 
+        //     this.config.map(c => c.groupKey === groupKeyValues.unique ? (b.piece.config.find(pC => pC.id === c.id) || c) : c)
+        // })})));
     }
-    // get stacks() {
-    //     console.log('CREATING STACKS')
-    //     return this.stacksData.map(s => s.map(b => {
-    //         console.log('b', b);
-    //         b.piece = this.generatePiece(b.piece.id, this.config.map(c => c.groupKey === groupKeyValues.unique ? (b.piece.config.find(pC => pC.id === c.id) || c) : c));
-    //         console.log('NO CONFIG, THE FUCK?')
-    //     }));
-    // }
-    setConfig = (newConfig: configT) => this.config = newConfig;
+    get stacks() {
+        console.log('CREATING STACKS', this.stacksData)
+        const newConfig = this.config;
+        const newStacks = this.stacksData.map(s => s.map(b => ({...b, piece: new Piece({id: b.piece.id, config: 
+            this.config.map(c => c.groupKey === groupKeyValues.unique ? (b.piece.config.find(pC => pC.id === c.id) || c) : c)
+        })})));
+        return newStacks;
+    }
+    setConfig = (newConfig: configT) => {
+        this.config = newConfig;   
+    };
     addStack = (stackIndex: number, piece: pieceI) => {
         if (piece) this.findAndRemoveBlock(piece.id)
-        this.stacks.splice(stackIndex, 0, [{ piece: piece || this.generatePiece() }]);
+        this.stacksData.splice(stackIndex, 0, [{ piece: piece || this.generatePiece() }]);
         this.clearEmptyStacks();
     };
     addToStack = (stackIndex: number, blockIndex: number, piece: pieceI) => {
-        console.log('add to stack piece', piece)
         if (piece) this.findAndRemoveBlock(piece.id);
-        this.stacks[stackIndex].splice(blockIndex, 0, { piece: piece || this.generatePiece() });
+        this.stacksData[stackIndex].splice(blockIndex, 0, { piece: piece || this.generatePiece() });
         this.clearEmptyStacks();
     };
     // util actions
     findAndRemoveBlock = (id: string) => {
-        const foundIndex = findIndex2D(this.stacks, id);
-        if (foundIndex) this.stacks[foundIndex.stack].splice(foundIndex.block, 1);
+        const foundIndex = findIndex2D(this.stacksData, id);
+        if (foundIndex) this.stacksData[foundIndex.stack].splice(foundIndex.block, 1);
     }
     generatePiece = (id?: string, config?: configT) => {
-        // unique config items are not observable/changable
-        // const values = {
-        //     globalConfig: this.config,
-        //     ...config && { pieceConfig: config },
-        // };
-        // const config2 = this.config.filter(c => c.groupKey !== groupKeyValues.unique).concat((config || []).filter(c => c.groupKey === groupKeyValues.unique))
-        return { id: id || generateId(), config: config || this.config.map(c => c.groupKey === groupKeyValues.unique ? { ...c } : c) }//overrideConfig(generatePieceConfig(values), this.config)}
+        return { id: id || generateId(), config: config || this.config.map(c => c.groupKey === groupKeyValues.unique ? { ...c } : c) }
     }
     clearEmptyStacks = () => {
-        this.stacks = this.stacks.filter(s => s.length > 0);
+        this.stacksData = this.stacksData.filter(s => s.length > 0);
     }
 }
