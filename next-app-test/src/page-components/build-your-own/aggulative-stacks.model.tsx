e9860622fe92af9f47636695bb306143f9cca87c
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2023-03-31 20:37:25 -0500

    cleaning

--- next-app-test/src/page-components/build-your-own/aggulative-stacks.model.tsx
+++ next-app-test/src/page-components/build-your-own/aggulative-stacks.model.tsx
@@ -1,7 +1,8 @@
 import { makeAutoObservable, makeObservable, observable, computed, action } from "mobx"
 
 import { groupKeyValues } from "-/Components/modifier/modifier.types";
-import { aggulativeStacksT, configT, pieceI } from "./build-your-own.types";
+import { aggulativeStacksT, configT } from "./build-your-own.types";
+import { isNum } from "-/util/helpers";
 
 const generateId = () => 'id' + (new Date()).getTime();
 const findIndex2D = (stacks: aggulativeStacksT, id: string) => {
@@ -12,14 +13,11 @@ const findIndex2D = (stacks: aggulativeStacksT, id: string) => {
     })
     return stack >= 0 ? { stack, block } : null;
 }
-const findBlock = (stacks: aggulativeStacksT, id: string) => {
-    return stacks.flat().find(b=> b.piece.id === id);
-}
 
 class Piece {
     id
     config
-    constructor({id, config}: {id: string, config: configT}){
+    constructor({ id, config }: { id: string, config: configT }) {
         this.id = id;
         this.config = config;
         makeObservable(this, {
@@ -33,7 +31,6 @@ class Piece {
 export class AggulativeStacks {
     config
     stacksData
-    // stacks
     constructor({ config, stacks: stacksData }: { config: configT, stacks: aggulativeStacksT }) {
         this.config = config;
         this.stacksData = stacksData;
@@ -48,42 +45,40 @@ export class AggulativeStacks {
         })
     }
     get stacks() {
-        return this.stacksData.map(s => s.map(b => ({...b, piece: new Piece({id: b.piece.id, config: 
-            this.config.map(c => c.groupKey === groupKeyValues.unique ? (b.piece.config.find(pC => pC.id === c.id) || c) : c)
-        })})));
+        return this.stacksData.map(s => s.map(b => ({
+            ...b,
+            piece: new Piece({
+                id: b.piece.id,
+                config:
+                    this.config.map(c => c.groupKey === groupKeyValues.unique ? (b.piece.config.find(pC => pC.id === c.id) || c) : c)
+            })
+        })));
     }
-    // setting actions
-    setConfig = (newConfig: configT) => {
-        this.config = newConfig;   
-    };
-    // generating actions
+    // model only util actions
     generatePiece = (id?: string, config?: configT) => {
         return { id: id || generateId(), config: config || this.config.map(c => c.groupKey === groupKeyValues.unique ? { ...c } : c) }
     }
+    // setting actions
+    setConfig = (newConfig: configT) => {
+        this.config = newConfig;
+    };
     // adding actions
     addStack = (stackIndex: number, pieceId?: string) => {
-        console.log("stuff", {stackIndex, pieceId});
-        let piece;
-        if (pieceId) {
-            piece = findBlock(this.stacksData, pieceId)?.piece;
-            this.findAndRemoveBlock(pieceId);
-        } 
+        const piece = pieceId ? this.findAndRemoveBlock(pieceId) : null;
         this.stacksData.splice(stackIndex, 0, [{ piece: piece || this.generatePiece() }]);
         this.clearEmptyStacks();
     };
     addToStack = (stackIndex: number, blockIndex: number, pieceId?: string) => {
-        let piece;
-        if (pieceId) {
-            piece = findBlock(this.stacksData, pieceId)?.piece;
-            this.findAndRemoveBlock(pieceId);
-        }
+        const piece = pieceId ? this.findAndRemoveBlock(pieceId) : null;
         this.stacksData[stackIndex].splice(blockIndex, 0, { piece: piece || this.generatePiece() });
         this.clearEmptyStacks();
     };
     // removal actions
     findAndRemoveBlock = (id: string) => {
-        const foundIndex = findIndex2D(this.stacksData, id);
-        if (foundIndex) this.stacksData[foundIndex.stack].splice(foundIndex.block, 1);
+        const i = findIndex2D(this.stacksData, id);
+        const piece = (i && isNum(i.stack) && isNum(i.block)) ? (this.stacksData[i.stack][i.block])?.piece : null;
+        if (i) this.stacksData[i.stack].splice(i.block, 1);
+        return piece;
     }
     clearEmptyStacks = () => {
         this.stacksData = this.stacksData.filter(s => s.length > 0);
