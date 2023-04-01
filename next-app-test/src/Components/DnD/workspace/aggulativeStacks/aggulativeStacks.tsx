d656e28f19f510314d73d37aeedfc8bce6c93733
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2023-03-31 19:19:25 -0500

    collect function closure keeping pieceId value between renders

--- next-app-test/src/Components/DnD/workspace/aggulativeStacks/aggulativeStacks.tsx
+++ next-app-test/src/Components/DnD/workspace/aggulativeStacks/aggulativeStacks.tsx
@@ -18,7 +18,9 @@ interface propsI {
 export const WorkspaceAggulativeStacks = observer(({ build, validation }: propsI) => {
     // using useState hook to track workspace piece dragging. BIG WHY: show/hiding <DropZone /> component shifts the DOM/mouse position of drag action, canceling React DnD's drag. FIX: Setting a timeout to let DnD's onDrag state 'solidify' before show/hiding
     const [isDraggingWorkspace, setIsDraggingWorkspace] = useState(false);
-    const { isDraggingDndLayer, draggingPieceId } = useDragLayer(monitor => {
+    // useing useState hook instead of React DnD item property as React DnD holds on to old workspace data (still not sure why)
+    const [draggingPieceId, setDraggingPieceId] = useState('')
+    const { isDraggingDndLayer } = useDragLayer(monitor => {
         console.log('DND PIECE', monitor.getItem()?.pieceId);
         return {
             isDraggingDndLayer: monitor.isDragging() && monitor.getItemType() === DnDItemTypes.ITEM,
@@ -28,7 +30,12 @@ export const WorkspaceAggulativeStacks = observer(({ build, validation }: propsI
     const isDragging = isDraggingWorkspace || isDraggingDndLayer;
     const onStackDrop = (stackIndex: number) => build.addStack(stackIndex, draggingPieceId)
     const onBlockDrop = (stackIndex: number, blockIndex: number) => build.addToStack(stackIndex, blockIndex, draggingPieceId)
-    const onBlockDrag = (isDraggingState: boolean) => setIsDraggingWorkspace(isDraggingState);
+    const onBlockDrag = (isDraggingState: boolean, pieceId?: string) => {
+        console.log('MY STATE: DRAGGING', pieceId);
+        if(pieceId) setDraggingPieceId(pieceId);
+        setIsDraggingWorkspace(isDraggingState);
+        
+    };
     return (<div className="flex a-i-end">
         {build?.stacks?.map((stack, index) => <Stack
             key={index}
@@ -45,7 +52,7 @@ export const WorkspaceAggulativeStacks = observer(({ build, validation }: propsI
 // TODO: move to another file
 interface dragZonePropsI {
     pieceId: string,
-    setIsDraggingState: (arg: boolean) => void,
+    setIsDraggingState: (isDragging: boolean, pieceId: string) => void,
     children: React.ReactNode
 }
 // TODO: generalize and share with freeformMatrix dropZone component
@@ -53,18 +60,16 @@ export const DragZone = observer(({ pieceId, setIsDraggingState, children }: dra
     console.log('----DragZone rerender', pieceId)
     const [, drag] = useDrag(() => ({
         type: DnDItemTypes.WORKSPACE_ITEM,
-        item: () => {
-            console.log('SETTING ITEM TO BE', pieceId);
-            return { pieceId: pieceId }
-        }, // HERE
         collect: (monitor) => {
-            if ( pieceId === monitor?.getItem()?.pieceId) setTimeout(() => setIsDraggingState(true), 250);
+            console.log('collect pieceId', pieceId);
+            if ( pieceId && monitor.isDragging()) setTimeout(() => setIsDraggingState(true, pieceId), 250);
             return { isDragging: !!monitor.isDragging() }
         },
         end: () => setTimeout(() => setIsDraggingState(false), 250)
     }), [])
     return <div ref={drag}>{children}</div>
 })
+
 export const DropZone = observer(({ onDrop }: { onDrop: () => void }) => {
     const [dropInfo, drop] = useDrop(() => ({
         accept: [DnDItemTypes.ITEM, DnDItemTypes.WORKSPACE_ITEM],
--- next-app-test/src/Components/DnD/workspace/aggulativeStacks/block.tsx
+++ next-app-test/src/Components/DnD/workspace/aggulativeStacks/block.tsx
@@ -9,7 +9,7 @@ interface propsI {
     block: blockI,
     isDragging: boolean,
     onDrop: (index: number) => void,
-    onDrag: (isDraggingState: boolean) => void
+    onDrag: (isDraggingState: boolean, pieceId?: string) => void
 }
 export const Block = observer(({ block, index, isDragging, onDrop, onDrag }: propsI) => {
     console.log('Block rerender', block);
