2d1e1b5577df14ad0b3f9900b08b898ac1e4efe3
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2023-11-13 13:56:17 -0600

    adding cypress back to tsconfig

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -1,11 +1,11 @@
-export {};
+export { }
 /// <reference types="cypress" />
 declare global{
   namespace Cypress {
     interface Chainable {
-      getByTestId(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
-      getByTestIdLike(dataTestPrefixAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
-      findByTestId(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
+      getByTestId(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>
+      getByTestIdLike(dataTestPrefixAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>
+      findByTestId(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>
     }
   }
 }
