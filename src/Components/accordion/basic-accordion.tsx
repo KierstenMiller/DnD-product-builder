44964622dbef0d6db4e9a130536f1f6d70620c6c
KierstenMiller <120688835+KierstenMiller@users.noreply.github.com>
2024-11-10 15:29:37 -0600

    implemented missing basic-accordion tests

--- src/Components/accordion/basic-accordion.tsx
+++ src/Components/accordion/basic-accordion.tsx
@@ -30,35 +30,37 @@ export const BasicAccordion = ({
     wrapper={children => <div data-testid={testId} className={styles('accordionContainer')}>{children}</div>}
   >
     <>
-        <ConditionalWrapper
-            condition={Boolean(headerLevel)}
-            // Need to provide a fallback for the headerLevel because typescript can't tell headerLevel will always be defined here
-            wrapper={children => <Header headerLevel={headerLevel ?? '1'} className={styles('accordionTriggerContainer')}>{children}</Header>}
+      <ConditionalWrapper
+        condition={Boolean(headerLevel)}
+        // Need to provide a fallback for the headerLevel because typescript can't tell headerLevel will always be defined here
+        wrapper={children => <Header headerLevel={headerLevel ?? '1'} className={styles('accordionTriggerContainer')}>{children}</Header>}
+      >
+        <button
+          data-testid={`${testId}-trigger`}
+          aria-expanded={isOpen}
+          className={classNames(styles('accordionTrigger'), { [`h${headerLevel}`]: headerLevel })}
+          aria-controls={`${id}-content`}
+          aria-disabled={disableCollapse}
+          id={id}
+          onClick={toggleDrawerVisibility}
         >
-            <button
-                aria-expanded={isOpen}
-                className={classNames(styles('accordionTrigger'), { [`h${headerLevel}`]: headerLevel })}
-                aria-controls={`${id}-content`}
-                aria-disabled={disableCollapse}
-                id={id}
-                onClick={toggleDrawerVisibility}
-            >
-                {headerText}{' '}
-                {!disableCollapse && <span className={styles('arrowContainer')}>
-                    <span className={classNames(styles('arrow'), isOpen ? styles('up') : styles('down'))} />
-                </span>}
-            </button>
-        </ ConditionalWrapper>
-        <div
-            id={`${id}-content`}
-            role="region"
-            aria-labelledby={id}
-            // including "display-none" css and the "hidden" attribute for older browsers. They do the same thing but it's good to have both
-            className={classNames(styles('accordionContent'), !isOpen && defaultStyles.hidden)}
-            hidden={!isOpen}
-        >
-            {children}
-        </div>
+          {headerText}
+          {!disableCollapse && <span className={styles('arrowContainer')}>
+            <span className={classNames(styles('arrow'), isOpen ? styles('up') : styles('down'))} />
+          </span>}
+        </button>
+      </ ConditionalWrapper>
+      <div
+        data-testid={`${testId}-content`}
+        id={`${id}-content`}
+        role="region"
+        aria-labelledby={id}
+        // including "display-none" css and the "hidden" attribute for older browsers. They do the same thing but it's good to have both
+        className={classNames(styles('accordionContent'), !isOpen && defaultStyles.hidden)}
+        hidden={!isOpen}
+      >
+        {children}
+      </div>
     </>
   </ConditionalWrapper>
 }
